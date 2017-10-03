4c0f64bb92174efc6ba80e55bcf3e5b90627d7d9
Chris Branch <cbranch@cloudflare.com>
2017-10-03 20:48:20 +0100

    Release Warp 2017.10.0

--- warp.rb
+++ warp.rb
@@ -1,9 +1,9 @@
 class Warp < Formula
   desc 'Cloudflare Warp'
   homepage 'https://warp.cloudflare.com'
-  url 'https://bin.equinox.io/a/kTvjvytNmY8/warp-2017.9.6-darwin-amd64.tar.gz'
-  sha256 'b6bd0b9fc87a3042193b936b08fd3a9bfcd7fcd65acc24c4c506a3c5e25cff8d'
-  version '2017.9.6'
+  url 'https://bin.equinox.io/a/2g3tCCeoHTF/warp-2017.10.0-darwin-amd64.tar.gz'
+  sha256 '4642c0cb7ab54adbf50071517ce470af4bebf6090d423ae5db4fa657fbc67fe8'
+  version '2017.10.0'
   def install
     bin.install 'cloudflare-warp'
   end
