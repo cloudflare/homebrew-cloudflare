9136b25801de7bb00adfcf1079fe86e7d67249c0
Chris Branch <cbranch@cloudflare.com>
2017-11-07 21:55:16 +0000

    Fix URL for Warp 2017.11.1

--- warp.rb
+++ warp.rb
@@ -1,7 +1,7 @@
 class Warp < Formula
   desc 'Cloudflare Warp'
   homepage 'https://warp.cloudflare.com'
-  url 'https://warp.cloudflare.com/downloads/warp-2017.11.1-darwin-amd64.tgz'
+  url 'https://warp.cloudflare.com/dl/warp-2017.11.1-darwin-amd64.tgz'
   sha256 'a2de37bfdabe8e6f538381c2fe694705701398522b8e597650ea895a19ab363b'
   version '2017.11.1'
   def install
