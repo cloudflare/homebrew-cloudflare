33a51168ef12f291d9a22f9246250b156d6d946d
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-04-28 10:33:06 +1000

    bump cf-terraforming to 0.2.0
    changelog: https://github.com/cloudflare/cf-terraforming/releases/tag/v0.2.0

--- Casks/cf-terraforming.rb
+++ Casks/cf-terraforming.rb
@@ -2,15 +2,27 @@ cask "cf-terraforming" do
   name "cf-terraforming"
   appcast "https://github.com/cloudflare/cf-terraforming/releases.atom"
   homepage "https://github.com/cloudflare/cf-terraforming"
-  version "0.1.1"
+  version "0.2.0"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_amd64.tar.gz"
-    sha256 "be29976d0814fe97848cf66baf35aaf84582cf714859ec427bdccc8fed384888"
-  else
-    url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_arm64.tar.gz"
-    sha256 "303c55b8140f3c876d2c0af3e1535f85ecb61fe7f3b175365b32a00c70daeea4"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_amd64.tar.gz"
+      sha256 "dc3dc22d38c32614f3c19fd48d829aee8e6f8ef001ed3e6e5cbcf24607a098c5"
+    else
+      url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_arm64.tar.gz"
+      sha256 "a1d22a187955e7237a49c2b67e4942be20c0d2d4f3c6ece3bee90dbf47cf961f"
+    end
   end
+  
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_linux_amd64.tar.gz"
+      sha256 "fa0627f53364f375bc67c2e7d736efe7eb8709773910220d84c8128f7ed4ff45"
+    else
+      url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_linux_arm64.tar.gz"
+      sha256 "f58dce7e42f851f97755f6a39728251f67f80710ce379a5f05f0da56290844f6"
+    end
+  end    
 
   binary "cf-terraforming"
 end
