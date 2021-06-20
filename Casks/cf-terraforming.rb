beecdb5a6888427d541c6dc9ff928aef95c50e31
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-06-21 09:09:38 +1000

    bump cf-terraforming to 0.3.0

--- Casks/cf-terraforming.rb
+++ Casks/cf-terraforming.rb
@@ -3,15 +3,15 @@ cask "cf-terraforming" do
   appcast "https://github.com/cloudflare/cf-terraforming/releases.atom"
   homepage "https://github.com/cloudflare/cf-terraforming"
   desc "cf-terraforming is a command line utility to facilitate terraforming your existing Cloudflare resources"
-  version "0.2.0"
+  version "0.3.0"
 
   if Hardware::CPU.intel?
     url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_amd64.tar.gz"
-    sha256 "dc3dc22d38c32614f3c19fd48d829aee8e6f8ef001ed3e6e5cbcf24607a098c5"
+    sha256 "757079471bb06ae0c7b74d05b9a59c8f494eb379711750efa53fe0737d486171"
   else
     url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_arm64.tar.gz"
-    sha256 "a1d22a187955e7237a49c2b67e4942be20c0d2d4f3c6ece3bee90dbf47cf961f"
-  end   
-
+    sha256 "8e25e9aec964393dbc2d6be6d1fb87db27a1e46926789944aca7d2bb0bfb01d6"
+  end
+  
   binary "cf-terraforming"
 end
