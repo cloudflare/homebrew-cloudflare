986306710f7a693cfa01d754da66a5f7bde78d39
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-09-01 09:40:06 +1000

    bump cf-terraforming to 0.4.0
    changelog: https://github.com/cloudflare/cf-terraforming/releases/tag/v0.4.0

--- Casks/cf-terraforming.rb
+++ Casks/cf-terraforming.rb
@@ -3,15 +3,15 @@ cask "cf-terraforming" do
   appcast "https://github.com/cloudflare/cf-terraforming/releases.atom"
   homepage "https://github.com/cloudflare/cf-terraforming"
   desc "cf-terraforming is a command line utility to facilitate terraforming your existing Cloudflare resources"
-  version "0.3.0"
+  version "0.4.0"
 
   if Hardware::CPU.intel?
     url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_amd64.tar.gz"
-    sha256 "757079471bb06ae0c7b74d05b9a59c8f494eb379711750efa53fe0737d486171"
+    sha256 "2e62ba2d0e6c96fe182525002d434c0ac90721264615eb886373629910fc8e4a"
   else
     url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_arm64.tar.gz"
-    sha256 "8e25e9aec964393dbc2d6be6d1fb87db27a1e46926789944aca7d2bb0bfb01d6"
+    sha256 "708f85987980829ff6dd883ea9b2149453fdc535c8acdb7c1312c90f3e24cd07"
   end
-  
+
   binary "cf-terraforming"
 end
