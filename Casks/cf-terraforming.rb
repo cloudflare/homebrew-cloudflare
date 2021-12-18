2d738f21457564675a6e1e26ff8a426fdba68d04
Sachin Fernandes <sachin@cloudflare.com>
2021-12-18 07:13:59 +0530

    Bump cf-terraforming to 0.6.1

--- Casks/cf-terraforming.rb
+++ Casks/cf-terraforming.rb
@@ -7,10 +7,10 @@ cask "cf-terraforming" do
 
   if Hardware::CPU.intel?
     url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_amd64.tar.gz"
-    sha256 "2ca219c1ab865c52a6cae7db059aac7b94130ad903f6020b1f741b438daa14f8"
+    sha256 "cad76567c1f0ba0266d950dbff3b42bbf423025eb73f967c7092e2ab71bc69be"
   else
     url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_arm64.tar.gz"
-    sha256 "b8b375e8e554ff6ff614b11bfeccf85f65520f1aa7fe132a286708381d97f703"
+    sha256 "741e617a3adb57d3d80f0b0f68c0a56c6afd2ddd7b58b0309d56befee18b66f3"
   end
 
   binary "cf-terraforming"
