094f44a4d1db64d4a22a01d6dc1f2ab92fcf9f0d
Sachin Fernandes <sachin@cloudflare.com>
2022-04-12 19:17:07 -0700

    Update cf-terraforming from 0.7.2 to 0.7.3

--- Casks/cf-terraforming.rb
+++ Casks/cf-terraforming.rb
@@ -3,14 +3,14 @@ cask "cf-terraforming" do
   appcast "https://github.com/cloudflare/cf-terraforming/releases.atom"
   homepage "https://github.com/cloudflare/cf-terraforming"
   desc "cf-terraforming is a command line utility to facilitate terraforming your existing Cloudflare resources"
-  version "0.7.2"
+  version "0.7.3"
 
   if Hardware::CPU.intel?
     url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_amd64.tar.gz"
-    sha256 "37a216b9ab6f939cd9c8ba56e2c8ba0c7b231f57d4f7ff6673c6577d73afe342"
+    sha256 "69c96d9d54a4a5ecf7a40fe098f6f8b675da8688517b0cec7c85232e1aded42e"
   else
     url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_arm64.tar.gz"
-    sha256 "ef513b34e24dbc906337edac100e18ab337cf87a1b824801c6a5e5cf9629dd2c"
+    sha256 "9530b3138565d3c720140c5f3847124ccd636ee959cf4058ecf846c1d91dd283"
   end
 
   binary "cf-terraforming"
