a328f4e39eef8036667447795ef9c0f56475a97d
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-03-27 13:12:01 +1100

    Update cf-terraforming from 0.10.0 to 0.12.0

--- Casks/cf-terraforming.rb
+++ Casks/cf-terraforming.rb
@@ -4,12 +4,12 @@ cask "cf-terraforming" do
   homepage "https://github.com/cloudflare/cf-terraforming"
   desc "cf-terraforming is a command line utility to facilitate terraforming your existing Cloudflare resources"
   binary "cf-terraforming"
-  version "0.10.0"
+  version "0.12.0"
   arch arm: "arm64", 
        intel: "amd64"
 
-  sha256 arm:   "504ffe9421c7d995746896d97ac0401663bbf45063cd25a3cd9bf2864549ec1e",
-         intel: "4998cce289ebf4129ce6ea0d2036c53e2bb884e92d906f2b085e7b585ca10346"
+  sha256 arm:   "2b94116df31f7b388a95dacbf4099099edd4817045eed570930f64bf5becd765",
+         intel: "aa60a8049a31e28738870a4f8a6f1bea6ceed44cbf564b964e95c6f765771ae1"
 
   url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_#{arch}.tar.gz"
 end
