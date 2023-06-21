95b1726b12113f801e9b421a2a3b5dc89c7b5bbe
Sudarsan Reddy <sreddy@cloudflare.com>
2023-06-21 17:03:59 +0100

    Release Cloudflare Tunnel 2023.6.1

--- cloudflared.rb
+++ cloudflared.rb
@@ -1,9 +1,9 @@
 class Cloudflared < Formula
   desc 'Cloudflare Tunnel'
   homepage 'https://developers.cloudflare.com/cloudflare-one/connections/connect-apps'
-  url 'https://packages.argotunnel.com/dl/cloudflared-2023.6.0-darwin-amd64.tgz'
-  sha256 'fbc21a5d0738377dab1dc1b71e7db8f8641751411786356abe2e9778e8353069'
-  version '2023.6.0'
+  url 'https://github.com/cloudflare/cloudflared/releases/download/2023.6.1/cloudflared-darwin-amd64.tgz'
+  sha256 'b2e8c01727bb7b1b8b974cf43651bee7e6c7e35470f0aea4fa67df0c0fcdaec8'
+  version '2023.6.1'
   def install
     bin.install 'cloudflared'
   end
