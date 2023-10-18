ef186fb4dd5af7b9353dce99660be889ff7f5679
Paul Mucur <mudge@mudge.name>
2023-10-18 20:20:58 +0100

    Upgrade cf-terraforming to 0.14.0 (#68)

--- Casks/cf-terraforming.rb
+++ Casks/cf-terraforming.rb
@@ -2,9 +2,9 @@ cask "cf-terraforming" do
   arch arm:   "arm64",
        intel: "amd64"
 
-  version "0.13.0"
-  sha256 arm:   "73755ab2277ab94a90dd29832af65abcc9bc22e110970c25a2ac16218f65a076",
-         intel: "910965fcf822242e96048744f128324b620518daf0aa75bfe76710e06c646ad9"
+  version "0.14.0"
+  sha256 arm:   "3b00ff4f8e2fad563f89efa445eed3e3f1e82b9eb97223a19aaad7df36d7b851",
+         intel: "3c8ff74054b3812d8b2163d7e1c34d3d67370b425dd5584d5f10a9cd61d4ad1a"
 
   url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_#{arch}.tar.gz"
   livecheck do
