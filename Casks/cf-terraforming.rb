b5e5dc8f7edf7834f6e0f54c8da8b799c1d89f33
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-10-30 14:58:10 +1100

    cf-terraforming 0.15.0 (#70)

--- Casks/cf-terraforming.rb
+++ Casks/cf-terraforming.rb
@@ -2,17 +2,18 @@ cask "cf-terraforming" do
   arch arm:   "arm64",
        intel: "amd64"
 
-  version "0.14.0"
-  sha256 arm:   "3b00ff4f8e2fad563f89efa445eed3e3f1e82b9eb97223a19aaad7df36d7b851",
-         intel: "3c8ff74054b3812d8b2163d7e1c34d3d67370b425dd5584d5f10a9cd61d4ad1a"
+  version "0.15.0"
+  sha256 arm:   "280305f9f31387bfb219f316b7917f399eac99cb84942a4c013f38d8d89f3a3b",
+         intel: "89e5009febb19ba0234132663516ccf8516c7d27f91cd83f5732bdf8a9738b18"
 
   url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_#{arch}.tar.gz"
-  livecheck do
-    url "https://github.com/cloudflare/cf-terraforming/releases.atom"
-  end
   name "cf-terraforming"
   desc "Utility to export your existing Cloudflare resources as Terraform resources"
   homepage "https://github.com/cloudflare/cf-terraforming"
+
+  livecheck do
+    url "https://github.com/cloudflare/cf-terraforming/releases.atom"
+  end
 
   binary "cf-terraforming"
 end
