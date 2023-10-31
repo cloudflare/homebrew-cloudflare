2f3196f4cd2572677704bd05b1d904029b3992af
Jacob Bednarz <jacob.bednarz@hey.com>
2023-10-31 00:50:40 +0000

    cf-terraforming 0.16.1

--- Casks/cf-terraforming.rb
+++ Casks/cf-terraforming.rb
@@ -2,9 +2,9 @@ cask "cf-terraforming" do
   arch arm:   "arm64",
        intel: "amd64"
 
-  version "0.15.0"
-  sha256 arm:   "280305f9f31387bfb219f316b7917f399eac99cb84942a4c013f38d8d89f3a3b",
-         intel: "89e5009febb19ba0234132663516ccf8516c7d27f91cd83f5732bdf8a9738b18"
+  version "0.16.1"
+  sha256 arm:   "55b8249fa33fa05b937d4294d20554ff1dd16cde93199cebf582223dda4b1fca",
+         intel: "2cbb51dad8d86b3e10a424333197c2b70831336cfa4f6c134fbcb3a0efba7fd2"
 
   url "https://github.com/cloudflare/cf-terraforming/releases/download/v#{version}/cf-terraforming_#{version}_darwin_#{arch}.tar.gz"
   name "cf-terraforming"
