96496629c71e63d9bbe0e1f2f4474ac83820fb38
Jacob Bednarz <jacob.bednarz@hey.com>
2024-03-22 20:15:00 +1100

    Updates cf-terraforming to 0.19.0

--- cf-terraforming.rb
+++ cf-terraforming.rb
@@ -1,8 +1,8 @@
 class CfTerraforming < Formula
   desc "Utility to export your existing Cloudflare resources as Terraform resources"
   homepage "https://github.com/cloudflare/cf-terraforming"
-  url "https://github.com/cloudflare/cf-terraforming/archive/refs/tags/v0.18.0.tar.gz"
-  sha256 "d8b15245ec971473ef3e3fc5d10abd9c60a0b027a5a494b71994a32f32178f4d"
+  url "https://github.com/cloudflare/cf-terraforming/archive/refs/tags/v0.19.0.tar.gz"
+  sha256 "7152e55a52ebc05255dfc3fe11e6665477d2ec5f43c04b56613d2ae745bc3899"
   license "Mozilla Public License 2.0"
   head "https://github.com/cloudflare/cf-terraforming.git", branch: "master"
 
