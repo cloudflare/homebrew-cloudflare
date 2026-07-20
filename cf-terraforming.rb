6b88d74934aaa36b6e43b80c6800d3cf8b070f56
Sarah Sicard <18204584+ssicard@users.noreply.github.com>
2026-07-20 16:08:29 -0500

    Updates cf-terraforming to 0.27.0

--- cf-terraforming.rb
+++ cf-terraforming.rb
@@ -1,8 +1,8 @@
 class CfTerraforming < Formula
   desc "Utility to export your existing Cloudflare resources as Terraform resources"
   homepage "https://github.com/cloudflare/cf-terraforming"
-  url "https://github.com/cloudflare/cf-terraforming/archive/refs/tags/v0.26.0.tar.gz"
-  sha256 "f2bd281ae08038efa858acbc7af937b305e5f4a042f81574cfc0922fde5cc8e8"
+  url "https://github.com/cloudflare/cf-terraforming/archive/refs/tags/v0.27.0.tar.gz"
+  sha256 "cd76f85a4a1664769521c870985ca12281c8c1c195d501430f80049ae59ed37d"
   license "Mozilla Public License 2.0"
   head "https://github.com/cloudflare/cf-terraforming.git", branch: "master"
 
